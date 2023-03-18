223d9d5d8669f32c165cadad84a895c3c498094c
MR X MIRROR <127676677+MR-X-MIRROR-BOTZ@users.noreply.github.com>
2023-03-18 15:13:31 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -168,38 +168,7 @@ def start(update, context):
             sendMarkup(text, context.bot, update.message, reply_markup)
 
 def restart(update, context):
-    cmd = update.effective_message.text.split(' ', 1)
-    dynoRestart = False
-    dynoKill = False
-    if len(cmd) == 2:
-        dynoRestart = (cmd[1].lower()).startswith('d')
-        dynoKill = (cmd[1].lower()).startswith('k')
-    if (not HEROKU_API_KEY) or (not HEROKU_APP_NAME):
-        LOGGER.info("If you want Heroku features, fill HEROKU_APP_NAME HEROKU_API_KEY vars.")
-        dynoRestart = False
-        dynoKill = False
-    if dynoRestart:
-        LOGGER.info("Dyno Restarting.")
-        restart_message = sendMessage("Dyno Restarting.", context.bot, update.message)
-        with open(".restartmsg", "w") as f:
-            f.truncate(0)
-            f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
-        heroku_conn = heroku3.from_key(HEROKU_API_KEY)
-        app = heroku_conn.app(HEROKU_APP_NAME)
-        app.restart()
-    elif dynoKill:
-        LOGGER.info("Killing Dyno. MUHAHAHA")
-        sendMessage("Killed Dyno.", context.bot, update.message)
-        alive.kill()
-        clean_all()
-        heroku_conn = heroku3.from_key(HEROKU_API_KEY)
-        app = heroku_conn.app(HEROKU_APP_NAME)
-        proclist = app.process_formation()
-        for po in proclist:
-            app.process_formation()[po.type].scale(0)
-    else:
-        LOGGER.info("Normally Restarting.")
-        restart_message = sendMessage("Normally Restarting.", context.bot, update.message)
+    restart_message = sendMessage("Normally Restarting.", context.bot, update.message)
         if Interval:
             Interval[0].cancel()
             Interval.clear()
